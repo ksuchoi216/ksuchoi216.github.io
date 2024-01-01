8e9cd8d24227ea98f94344d07cc44611e4599f3c
ksuchoi216 <ksuchoi216@gmail.com>
2024-01-01 15:14:42 +0900

    pushed

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "과거 시간에 영향 받지 않는 정상성 - ARIMA (1)",
+        "excerpt":"시계열 분석 및 예측에 기초 중 기초 정상성에 대해 알아봅시다.이 글을 읽는다면 아래 내용을 얻어가게 됩니다. 정상성이 무엇인지 그리고 왜 필요한지 어떻게 정상성을 확인 할 수 있는지 정상성인 시계열 데이터를 다루는 법들어가기 전에, 새로운 용어가 나왔을 때는 어원이나 한자를 보는게 이해에 도움이 된다.한자로는 정할 정에 항상 항을 항상 변한지 않는다(영어로는...","categories": ["ARIMA"],
+        "tags": ["None"],
+        "url": "http://localhost:4000/arima/2023/12/17/p0-stationary.html"
+      },{
+        "title": "ARIMA는 뭔가요? - ARIMA (2)",
+        "excerpt":"이 글을 읽는다면 아래 내용을 얻어가게 됩니다. ARIMA가 무엇인지 자기회기(AR) 모델이 무엇인지 이동평균(MA) 모델이 무엇인지 ARIMA 파이썬 실행법ARIMA ARIMA 모델을 이해하기 위해 영어 약자를 풀어 설명하면 쉬워요. AutoRegressive Integrated Moving Average = AutoRegressive + Moving Average를 합친 모델이구나. 우린 이제 Autoregressive 모델과 Moving average 모델이 무엇인지 알기만 하면 됩니다. 시계열...","categories": ["None"],
+        "tags": ["None"],
+        "url": "http://localhost:4000/none/2023/12/17/p1-arima.html"
+      },{
+        "title": "None",
+        "excerpt":"[What to learn?] 이 글을 읽는다면 아래 내용을 얻어가게 됩니다.           [Title]     [What?]     [Why?]     [How?]     지금까지 배운 것 복습!     참고 자료   ","categories": ["None"],
+        "tags": ["None"],
+        "url": "http://localhost:4000/none/2023/12/17/p2-fcst.html"
+      },]
---
+++ add.py
@@ -0,0 +1,89 @@
+import argparse
+from types import SimpleNamespace
+from datetime import datetime
+from PIL import Image
+
+import os
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="add blog post file")
+    parser.add_argument("--date", "-d", help="put yymmdd", type=str)
+    parser.add_argument("--keyword", "-k", help="put keyword", type=str)
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    if args.keyword is None:
+        raise Exception("No Keyword!: put keyword by using -k")
+    else:
+        keyword = args.keyword
+
+    if args.date is not None:
+        dt = args.date
+        # if len(dt) != 6:
+        # raise print("break")
+        year = int("20" + dt[:2])
+        month = int(dt[2:4])
+        day = int(dt[4:])
+        dt = datetime(year, month, day)
+        post_date = dt.strftime("%Y-%m-%d")
+        # img_date = dt.strftime("%Y%m%d")
+    else:
+        post_date = datetime.now().strftime("%Y-%m-%d")
+        # img_date = datetime.now().strftime("%Y%m%d")
+
+    img_dir = f"./images/{keyword}"
+    img_path = os.path.join(img_dir, f"main.jpg")
+
+    # * image
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir)
+    else:
+        print(f"The image directory already exists: {img_dir}")
+
+    if not os.path.isfile(img_path):
+        img = Image.new("RGB", (300, 150))  # (width, height)
+        img.save(img_path)
+    else:
+        print(f"The image already exists: {img_path}")
+
+    # * post
+    lines = [
+        "---",
+        f"title: '{keyword}'",
+        "excerpt_separator: '<!--more-->'",
+        "categories:",
+        "   - None",
+        "tags:",
+        "   - None",
+        "# image:",
+        f"#     path: {img_path}",
+        f"#     thumbnail: {img_path}",
+        f"#     caption: ",
+        f"# hidden: true",
+        "---",
+        "[What to learn?] ",
+        "이 글을 읽는다면 아래 내용을 얻어가게 됩니다.",
+        "* ",
+        "<!--more-->",
+        "## [Title]",
+        "### [What?] ",
+        "### [Why?] ",
+        "### [How?] ",
+        "## 지금까지 배운 것 복습!",
+        "## 참고 자료",
+    ]
+    post_path = os.path.join("./_posts", f"{post_date}-{keyword}.md")
+
+    if not os.path.exists(post_path):
+        with open(post_path, "w") as f:
+            f.write("\n".join(lines))
+    else:
+        print(f"The markdown post file already exists: {post_path}")
+
+    # with open(post_path, "w") as f:
+    #     f.write("\n".join(lines))
