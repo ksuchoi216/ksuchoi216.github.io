deaab4dd6526e85af8fa35a7929976e653dbbe64
KC <ksuchoi216@gmail.com>
2024-01-21 16:38:46 +0900

    updated

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"시계열 분석 및 예측에 기초 중 기초, 정상성에 대해 알아봅시다.이 글을 읽는다면 아래 내용을 얻어가게 됩니다. 정상성이 무엇인지 그리고 왜 필요한지 어떻게 정상성을 확인 할 수 있는지 정상성인 데이터를 비정상성으로 만드는 법들어가기 전에, 새로운 용어가 나왔을 때는 어원이나 한자를 보는게 이해에 도움이 됩니다.한자로는 정할 정에 항상 항을 항상 변하지 않는다(영어로는...","categories": ["data-science"],
         "tags": ["time-series"],
         "url": "http://localhost:4000/data-science/2023/01/13/stationary.html"
+      },{
+        "title": "Autoformer",
+        "excerpt":"일단 Autoformer는 기존에 시계열 예측에 사용된 트랜스포머 모델에 시계열 순서 정보를 잃어버리는 고질적 문제를 해결하기 위해 제안되었습니다. 트랜스포머 계열은 시계열 데이터 본래 특징을 잃어버립니다. 시간 순서대로 연결되어 있는 데이터가 시계열 데이터인데 이러한 정보를 트랜스포머 모델들은 반영하지 못합니다. 그렇다면 어떻게 autoformer가 해결했는지 탐험해보겠습니다. What is Autoformer? Autoformer는 Auto-correlation + Transformer을 결합한...","categories": ["data-science"],
+        "tags": ["time-series","transformer"],
+        "url": "http://localhost:4000/data-science/2023/01/21/autoformer.html"
       },]
