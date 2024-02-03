bb300c1e9886168f4a936202b15f1fe5894ac0d4
KC <ksuchoi216@gmail.com>
2024-02-04 00:12:32 +0900

    updated

--- obsidian.py
+++ obsidian.py
@@ -60,7 +60,7 @@ def modify_lines(args, lines):
             img_file = re.search(r"\[\[(.+?)\]\]", line).group(1)
             old_img_path = f"{args.old_dir}/attachments/{img_file}"
             new_img_path = f".{IMG_BASE}/{img_file}"
-            lines[i] = f"![{img_file}]({new_img_path}){{:, .align-center}}"
+            lines[i] = f"![{img_file}]({new_img_path}){{:, .align-center}}\n"
             args.img_paths.append([old_img_path, new_img_path])
 
     return args, lines
